d6e98de560972051c02eac8e61d4d1825501f62a
Adnan <amadda96@gmail.com>
2025-12-17 01:48:25 -0600

    first commit

---
+++ src/main/java/com/carapp/model/Customer.java
@@ -0,0 +1,52 @@
+package com.carapp.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "customers")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String phone;
+
+    private String address;
+
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    @OneToMany(mappedBy = "customer", cascade = cascadeType.ALL, orphanRemoval = true)
+    private List<Vehicle> vehicles = new ArrayList<>();
+
+    @OnetoMany(mappedBy = "customer", cascade = CascadeType.All, orphanRemoval = true)
+    private List<Transaction> transactions = new ArrayList<>();
+
+}
---
+++ src/main/java/com/carapp/model/Transaction.java
@@ -0,0 +1,4 @@
+package com.carapp.model;
+
+public class Transaction {
+}
